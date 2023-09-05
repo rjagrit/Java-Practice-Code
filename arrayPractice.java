aa114ee5c1aec6c4682dca449e408ce11c6dcb05
Jagrit <rjagrit.2k@gmail.com>
2023-09-05 23:58:00 +0530

    Day-2

---
+++ arrayPractice.java
@@ -0,0 +1,113 @@
+import javax.swing.*;
+
+/*
+  ----------declaration of an array----------
+  1) data_type array_name[];
+  2) data_type[] array_name;
+
+    int myAry[];
+
+  ----------Initialization of an array----------
+  Gives memory to an array
+  (When we create an array we need size and the array size will be fixed, we cannot change)
+
+   myAry[]= new int[5];
+
+   ----------Declaration and Initilization----------
+   dataType[] arrayName= new dataType[size];
+   dataType arrayName[]= new dataType[size];
+
+   int[] myAry= new int[5];
+   int myAry[]= new int[5];
+   int[] myAry= {5,4,8,9};
+ */
+
+/* --------1st Pgm----------
+ int[] myArray= new int[5];
+ myArray[0]=5;
+ myArray[1]=4;
+ myArray[2]=3;
+ myArray[3]=2;
+ myArray[4]=7;
+
+ System.out.println("Array elements are:");
+ for(int i=0;i<5;i++)
+ {
+     System.out.println("Value at Index "+i+" is = "+myArray[i]);
+ }
+ ----------------------------
+ */
+/*
+---------2nd Pgm-------------
+public void printArray(int arr[])
+        {
+        int n= arr.length;
+        for(int i=0; i<n;i++)
+        {
+        arr[i]=i;
+        System.out.println("Index "+i+" value = "+arr[i]);
+        }
+        }
+
+public void arrayDemo()
+        {
+        int myAry[]=new int[5];
+        printArray(myAry);
+//      function call
+        }
+    public static void main(String [] args)
+    {
+    arrayPractice obj= new arrayPractice();
+    obj.arrayDemo();
+    }
+ */
+
+//public class arrayPractice {
+//
+//    public static void main(String [] args)
+//    {
+//System.out.println("Hi");
+//System.out.println("Hi");
+//    }
+//}
+
+// Program to find vowel , consonant, whitespace, and digits from a given strings
+
+class Stringss
+{
+    public static void main(String []args){
+        String s="uiet is an Engineering college since 2004";
+        s=s.toLowerCase();
+        int v=0,c=0,d=0,u=0;
+        char arr[]= new char[s.length()];
+        for (int i=0;i<s.length();i++)
+        {
+//            storing each word at the index values
+            arr[i]=s.charAt(i);
+        }
+        for (int i=0;i<s.length();i++)
+        {
+//            storing each word at the index values
+            if(arr[i]>='a' && arr[i]<='z')
+            {
+                if (arr[i] == 'a' || arr[i] == 'e' || arr[i] == 'i' || arr[i] == 'o' || arr[i] == 'u') {
+                    v++;
+                } else {
+                    c++;
+                }
+            }
+            else if(arr[i]>='0' && arr[i]<='9')
+            {
+                d++;
+            }
+            else if(arr[i]==' ')
+            {
+                u++;
+            }
+        }
+            System.out.println(v);
+            System.out.println(c);
+            System.out.println(d);
+            System.out.println(u);
+        }
+    }
