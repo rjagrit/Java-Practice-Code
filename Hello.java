79e804ba8f57523b8cdb390a28551ab9286b9850
Jagrit <rjagrit.2k@gmail.com>
2023-09-05 23:29:14 +0530

    Day-1

--- Hello.java
+++ Hello.java
@@ -1,6 +1,6 @@
 public class Hello {
     public static void main(String[] args)
     {
-        System.out.println("Hello, I am ready for the Journey now!");
-    }
+        System.out.println("Hello, I am ready for the Journey now!!");
+    }s
 }
