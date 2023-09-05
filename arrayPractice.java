45657b0d8a62f809b5f7ef2dba4eccc1989cf7e3
Jagrit <rjagrit.2k@gmail.com>
2023-09-06 00:02:50 +0530

    Day-2

--- arrayPractice.java
+++ arrayPractice.java
@@ -73,41 +73,9 @@ public void arrayDemo()
 
 // Program to find vowel , consonant, whitespace, and digits from a given strings
 
-class Stringss
+class arrayPractice
 {
     public static void main(String []args){
-        String s="uiet is an Engineering college since 2004";
-        s=s.toLowerCase();
-        int v=0,c=0,d=0,u=0;
-        char arr[]= new char[s.length()];
-        for (int i=0;i<s.length();i++)
-        {
-//            storing each word at the index values
-            arr[i]=s.charAt(i);
-        }
-        for (int i=0;i<s.length();i++)
-        {
-//            storing each word at the index values
-            if(arr[i]>='a' && arr[i]<='z')
-            {
-                if (arr[i] == 'a' || arr[i] == 'e' || arr[i] == 'i' || arr[i] == 'o' || arr[i] == 'u') {
-                    v++;
-                } else {
-                    c++;
-                }
-            }
-            else if(arr[i]>='0' && arr[i]<='9')
-            {
-                d++;
-            }
-            else if(arr[i]==' ')
-            {
-                u++;
-            }
-        }
-            System.out.println(v);
-            System.out.println(c);
-            System.out.println(d);
-            System.out.println(u);
+
         }
     }
