75b2ca81b1c0b7930d19ede95cefec850dd3a709
Jagrit <rjagrit.2k@gmail.com>
2023-10-19 00:32:31 +0530

    Day-9

--- ArithmeticOperations.java
+++ ArithmeticOperations.java
@@ -1,27 +1,49 @@
 import java.util.Scanner;
 public class ArithmeticOperations
 {
+
     public static void main(String [] args)
     {
-        int fnum,snum,add,sub,mul,div,option;
+        int fnum,snum,add,sub,mul,div,option,result;
         Scanner obj= new Scanner(System.in);
         System.out.print("Enter first number:");
         fnum= obj.nextInt();
         System.out.print("Enter second number:");
         snum= obj.nextInt();
-        System.out.println("Operations are \n1.Addition\n2.Subtraction\n3.Multiplication\n1.Division");
-        System.out.println("Press 1 for Additon Operation");
-        System.out.println("Press 2 for Subtraction Operation");
-        System.out.println("Press 3 for Multiplication Operation");
-        System.out.println("Press 4 for Divison Operation");
-        System.out.print("Your Option: ");
-        option=obj.nextInt();
+
         while(true)
         {
+            System.out.println("Operations are \n1.Addition\n2.Subtraction\n3.Multiplication\n1.Division");
+            System.out.print("Your Option: ");
+            option=obj.nextInt();
+
+            switch (option)
+            {
+                case 1:
+                    result = fnum+snum;
+                    System.out.println("Addition of "+fnum+" and "+snum+" : "+result+"\n");
+                    break;
+
+                case 2:
+                   result = fnum-snum;
+                    System.out.println("Subtraction of "+fnum+" and "+snum+" : "+result+"\n");
+                    break;
+
+                case 3:
+                    result = fnum*snum;
+                    System.out.println("Multiplication of "+fnum+" and "+snum+" : "+result+"\n");
+                    break;
+
+                case 4:
+                    result = fnum/snum;
+                    System.out.println("Division of "+fnum+" and "+snum+" : "+result+"\n");
+                    break;
+
+                default:
+                    System.exit(0);
+            }
 
         }
-
-
 
 
     }
